79acb03af4246e1fdbccc0664dddb34cd9abb5e4
Alex Carvalho <twilight.vulpine@gmail.com>
2014-01-03 11:21:03 -0500

    Config file added

---
+++ config.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/ruby
+
+# Symbols Module
+# Author :: Alex Carvalho
+
+#
+# 
+#
+#
+
+module IRCBotConfig
+
+    NICK = "BondsBot" 
+    ALTNICK = ["BondsIRCBot"]
+    
+    NICKSERV_ACCOUNT = ""
+    NICKSERV_PASSWORD = ""
+    
+    SERVER = "irc.canternet.org"
+    PORT = 6669
+    
+    AUTOJOIN_CHANNELS = {
+        
+        
+    }
+    
+    USERS = {
+        :admin => [
+                
+        ], 
+        :vip => [
+            
+        ],
+        :non_grata => [
+            
+        ],
+        :banned => [
+        
+        ]
+    }
+    
+
+end
